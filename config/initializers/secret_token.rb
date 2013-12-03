dec70a2f59aeadfd20df2194edc44b8845f18a4a
Basma Alkerm <eng.balkerm@gmail.com>
2013-12-03 03:39:21 +0200

    the complete required app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudentTracker::Application.config.secret_token = '3c2234b28eae914ab12d4784f0af6a4d567e95d0e1b0d8974e87f0fe2a363696048d46c08f5bd254d6464aba103f9613ebf22f1442c468f9fb0979f4129e82ac'
